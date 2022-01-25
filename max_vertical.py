c5955fa9648fc5179c173a9f747681d4dc264cc3
USER <user@dyn3187-247.wlan.ic.ac.uk>
2022-01-25 21:13:30 +0000

    added all_vert and max_vert

--- max_vertical.py
+++ max_vertical.py
@@ -1,16 +1,21 @@
-def sort_vertical(photo_set):
+from slide import Slide
+
+
+def max_vertical(photo_set):
     # photo.id = int
     # photo.tags = set()
     max_tags = 0
-    slides = {}
+    slides = set()
     for photo1 in photo_set:
         slide = Slide()
         for photo2 in photo_set:
+            if photo1 == photo2:
+                continue
             total_tags = photo1.tags.intersection(photo2.tags)
             if len(total_tags) > max_tags:
                 max_tags = total_tags
                 slide.ids = [photo1.id, photo2.id]
-        slides.append(slide)
+        slides.add(slide)
         photo_set.remove(photo1)
         photo_set.remove(photo2)
     return slides
