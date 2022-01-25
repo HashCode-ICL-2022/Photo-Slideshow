5830efd918b000a9acb67e5748bb2604092e1728
Loumstar <louis.manestar@gmail.com>
2022-01-25 21:12:45 +0000

    Added image and slide classes. Given main script for parsing

---
+++ structures/image.py
@@ -0,0 +1,16 @@
+import re
+
+image_string_re = re.compile(r"^(?P<orientation>\w)\s(?P<ntags>\d+)\s(?P<tags>[\w\s]+)$")
+
+class Image:
+    
+    def __init__(self, image_string, id):
+        self.id = id
+
+        match = image_string_re.match(image_string)
+        
+        if match is None:
+            raise ValueError("Image string didn't match")
+
+        self.orientation = match.group("orientation")
+        self.tags = match.group("tags").strip().split(" ")
